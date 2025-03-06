aa3c72b7df2e378f24a6d77b6ce7c3b025394ad5
nikulsinhhadiyol0 <nikulsinhhadiyol0@gmail.com>
2025-03-06 09:48:03 +0530

    feat: handle single number input

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -3,3 +3,7 @@ const add = require("../src/calculator");
 test("returns 0 for an empty string", () => {
     expect(add("")).toBe(0);
 });
+
+test("returns the number itself for a single input", () => {
+    expect(add("1")).toBe(1);
+});
